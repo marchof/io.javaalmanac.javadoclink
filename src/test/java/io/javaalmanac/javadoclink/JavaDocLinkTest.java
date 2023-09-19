2b40a1ea9db75f0b3c4a90b85787f9c8ea49c9a9
Marc R. Hoffmann <hoffmann@mountainminds.com>
2023-09-19 07:15:52 +0200

    Use Java 21 Javadoc location for reference (#7)

--- src/test/java/io/javaalmanac/javadoclink/JavaDocLinkTest.java
+++ src/test/java/io/javaalmanac/javadoclink/JavaDocLinkTest.java
@@ -124,7 +124,7 @@ public abstract class JavaDocLinkTest {
 		JDK_API_DOC.put("18", "https://docs.oracle.com/en/java/javase/18/docs/api/");
 		JDK_API_DOC.put("19", "https://docs.oracle.com/en/java/javase/19/docs/api/");
 		JDK_API_DOC.put("20", "https://docs.oracle.com/en/java/javase/20/docs/api/");
-		JDK_API_DOC.put("21", "https://download.java.net/java/early_access/jdk21/docs/api/");
+		JDK_API_DOC.put("21", "https://docs.oracle.com/en/java/javase/21/docs/api/");
 		JDK_API_DOC.put("22", "https://download.java.net/java/early_access/jdk22/docs/api/");
 	}
 
