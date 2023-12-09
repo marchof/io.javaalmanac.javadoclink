a8e71adfd862fa98fa5a5a334a6651907aa692a8
Marc R. Hoffmann <hoffmann@mountainminds.com>
2023-12-09 18:09:02 +0100

    Add support for Java 23

--- src/main/java/io/javaalmanac/javadoclink/JavaDocLinkImpl.java
+++ src/main/java/io/javaalmanac/javadoclink/JavaDocLinkImpl.java
@@ -85,6 +85,7 @@ final class JavaDocLinkImpl implements JavaDocLink {
 		VERSIONS.put("20", VERSIONS.get("11"));
 		VERSIONS.put("21", VERSIONS.get("11"));
 		VERSIONS.put("22", VERSIONS.get("11"));
+		VERSIONS.put("23", VERSIONS.get("11"));
 	}
 
 	private final String base;
